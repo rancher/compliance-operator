a5185869195079daf4815864f37394fda067cbf4
Teja78906 <alluteja@Allus-MacBook-Air.local>
2026-03-30 14:23:16 +0530

    [main]CI improvements

--- hack/make/tools.mk
+++ hack/make/tools.mk
@@ -12,36 +12,47 @@ endif
 K3D = $(TOOLS_BIN)/k3d-$(K3D_VERSION)
 $(K3D):
 	rm -f $(TOOLS_BIN)/k3d*
-	curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | \
-		PATH=$(PATH):$(TOOLS_BIN) K3D_INSTALL_DIR="$(TOOLS_BIN)" TAG="$(K3D_VERSION)" USE_SUDO=false bash
-	mv $(TOOLS_BIN)/k3d $(TOOLS_BIN)/k3d-$(K3D_VERSION)
+	curl -sSfL -o $(K3D) "https://github.com/k3d-io/k3d/releases/download/$(K3D_VERSION)/k3d-$(OS_NAME)-$(OS_ARCH)"
+	K3D_SUM_VAR=K3D_SUM_$(OS_ARCH); \
+	eval SUM=\$$K3D_SUM_VAR; \
+	echo "$$SUM  $(K3D)" | shasum -a 256 -c -
+	chmod u+x $(K3D)
 
 GOLANGCI = $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)
 $(GOLANGCI):
-	rm -f $(TOOLS_BIN)/golangci-lint*
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(TOOLS_BIN) $(GOLANGCI_VERSION)
-	mv $(TOOLS_BIN)/golangci-lint $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)
+	rm -rf $(TOOLS_BIN)/golangci*                         # cleanup BEFORE installing
+	curl -sSfL -o $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz \
+		"https://github.com/golangci/golangci-lint/releases/download/v$(GOLANGCI_VERSION)/golangci-lint-$(GOLANGCI_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz"
+	echo "$(GOLANGCI_SUM_$(OS_ARCH))  $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz" \
+		| shasum -a 256 -c -
+	tar -xf $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz -C $(TOOLS_BIN)
+	mv $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)-$(OS_NAME)-$(OS_ARCH)/golangci-lint $(GOLANGCI)
+	chmod u+x $(GOLANGCI)
 
 KUBECTL = $(TOOLS_BIN)/kubectl-$(KUBECTL_VERSION)
 $(KUBECTL):
 	rm -f $(TOOLS_BIN)/kubectl*
-	curl --output $(KUBECTL) -sSfL "https://dl.k8s.io/release/v$(KUBECTL_VERSION)/bin/$(OS_NAME)/$(OS_ARCH)/kubectl"
-	echo "$(shell curl -L "https://dl.k8s.io/release/v$(KUBECTL_VERSION)/bin/$(OS_NAME)/$(OS_ARCH)/kubectl.sha256")  $(KUBECTL)" | shasum -a 256 -c -
+	curl -sSfL -o $(KUBECTL) \
+		"https://dl.k8s.io/release/$(KUBECTL_VERSION)/bin/$(OS_NAME)/$(OS_ARCH)/kubectl"
+	KUBECTL_SUM_VAR=KUBECTL_SUM_$(OS_ARCH); \
+	eval SUM=\$$KUBECTL_SUM_VAR; \
+	echo "$$SUM  $(KUBECTL)" | shasum -a 256 -c -
 	chmod u+x $(KUBECTL)
 
 HELM = $(TOOLS_BIN)/helm-$(HELM_VERSION)
 $(HELM):
 	rm -rf $(TOOLS_BIN)/helm*
 	mkdir -p $(TOOLS_BIN)/tmp-helm
-	curl --output $(TOOLS_BIN)/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz -sSfL "https://get.helm.sh/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz"
-	$(call indirect-value,HELM_SUM)
-	cd $(TOOLS_BIN) && echo "$(shell curl -L "https://get.helm.sh/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz.sha256sum")" | shasum -a 256 -c -
-	tar -xf $(TOOLS_BIN)/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz --strip-components 1 -C $(TOOLS_BIN)/tmp-helm
+	curl -sSfL -o $(TOOLS_BIN)/helm.tar.gz \
+		"https://get.helm.sh/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz"
+	HELM_SUM_VAR=HELM_SUM_$(OS_ARCH); \
+	eval SUM=\$$HELM_SUM_VAR; \
+	echo "$$SUM  $(TOOLS_BIN)/helm.tar.gz" | shasum -a 256 -c -
+	tar -xf $(TOOLS_BIN)/helm.tar.gz --strip-components 1 -C $(TOOLS_BIN)/tmp-helm
 	mv $(TOOLS_BIN)/tmp-helm/helm $(HELM)
 	chmod u+x $(HELM)
-	rm -rf $(TOOLS_BIN)/helm-$(HELM_VERSION)-$(OS_NAME)-$(OS_ARCH).tar.gz $(TOOLS_BIN)/tmp-helm
+	rm -rf $(TOOLS_BIN)/helm.tar.gz $(TOOLS_BIN)/tmp-helm
 
-# go-install-tool will 'go install' any package $2 and install it as $1.
 define go-install-tool
 @[ -f $(1) ] || { \
 set -e ;\
